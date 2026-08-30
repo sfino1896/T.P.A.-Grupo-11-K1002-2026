810a64fb76bce30a7d78402d56972c133c4a63f1
sfino <sfino@frba.utn.edu.ar>
2026-08-30 20:48:41 -0300

    Funcion encriptarYValidar

---
+++ ventas.cpp
@@ -0,0 +1,49 @@
+using namespace std;
+
+#include <cstring> 
+
+
+const int CORRIMIENTO = 5;
+
+
+
+struct Mozo
+{
+    int idMozo; 
+    char nombre[50]; 
+    char password[20]; 
+    float totalComision;
+};
+
+struct Comanda
+{
+    int idMozo;
+    int codigoProducto;
+    int cantidad;
+    float comision;
+};
+
+
+bool encriptarYValidar(char clave[], int len, Mozo mozo)
+{
+
+
+    char claveEncriptada[len];
+
+    int i = 0;
+    while (i <= len)
+    {
+       claveEncriptada[i] = clave[i] + CORRIMIENTO;
+       i++;
+    }
+
+    return (strcmp(claveEncriptada, mozo.password) == 0); 
+       
+}
+
+int main(int argc, char const *argv[])
+{
+
+    return 0;
+    
+}
